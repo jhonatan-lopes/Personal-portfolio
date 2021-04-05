80dc3285214e3cd1799bbdf644a69382d177ecc5
Jhonatan Da Ponte Lopes <jhonatan_plopes@hotmail.com>
2021-04-05 15:21:37 +0100

    Changed fonts

--- main/views.py
+++ main/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from .models import Education, Expertise, Experience
 
 # Create your views here.
 def home(request):
     return render(request,"main/home.html")
 
 def about(request):
-    return render(request, "main/about.html")
+    about_context = {}
+
+    about_context["educations"] = Education.objects.all().order_by("-end_date")
+    about_context["expertises"] = Expertise.objects.all()
+    about_context["experiences"] = Experience.objects.all().order_by("end_date")
+
+    return render(request, "main/about.html", context=about_context)
+
